560ed5f1eb9cf11d952e6265d18d5d017261da43
mmecik <michael.mecik@informatik.hs-fulda.de>
2024-03-08 23:46:30 +0100

    * Uncomment lines for validation data extraction

--- scripts/extract_imagenet.sh
+++ scripts/extract_imagenet.sh
@@ -30,8 +30,8 @@ cd ..
 #
 # Extract the validation data and move images to subfolders:
 #
-#mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
-#wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
+mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
+wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
 #
 # Check total files after extract
 #
