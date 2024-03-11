56674a64568598b1658149a60d1a7c64c88a759d
mmecik <michael.mecik@informatik.hs-fulda.de>
2024-03-11 15:27:15 +0100

    * Checkpoint

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ from tqdm import tqdm
 if __name__ == "__main__":
     modelbuilder = ModelBuilder()
     dataset = DatasetBuilder.build(DatasetTypes.IMAGENET, "configs/vgg19/quant_config.yaml")
-    #validator = Validator()
+    validator = Validator()
 
     quant_model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/quant_config.yaml", preload_weights=True)
-    model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/config.yaml", preload_weights=False)
+    model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/config.yaml", preload_weights=True)
 
-    Tuner.tune(quant_model, torch.optim.SGD, torch.nn.CrossEntropyLoss(), dataset, 5, 10, 10)
+    #Tuner.tune(quant_model, torch.optim.SGD, torch.nn.CrossEntropyLoss(), dataset, 5, 10, 10)
 
     #trainer = Trainer()
     #trainer.train(quant_model, None, dataset, torch.nn.CrossEntropyLoss(), torch.optim.SGD(quant_model.parameters()), 0.001)
 
-    #validator.validate(vgg19, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
+    #validator.validate(quant_model, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
+    validator.validate(model, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
