510710117ce29047dd5754671689d14c20066977
carelledjuffo <djuffocarelle@yahoo.com>
2024-04-01 14:01:06 +0200

    - uncomment block code

--- main.py
+++ main.py
@@ -10,27 +10,18 @@ import torchvision.models as models
 import time
 from tqdm import tqdm
 
-
 if __name__ == "__main__":
-   # modelbuilder = ModelBuilder()
-   # dataset = DatasetBuilder.build(DatasetTypes.IMAGENET, "configs/vgg19/quant_config.yaml")
-   # validator = Validator()
-   mobilenet = models.mobilenet_v3_large(pretrained=True)
-   vgg = models.vgg19(pretrained=True)
-   resnet18 = models.resnet18(pretrained=True)
+    modelbuilder = ModelBuilder()
+    dataset = DatasetBuilder.build(DatasetTypes.IMAGENET, "configs/vgg19/quant_config.yaml")
+    validator = Validator()
 
-   # print(mobilenet)
-   print(resnet18)
+    quant_model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/quant_config.yaml", preload_weights=True)
+    model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/config.yaml", preload_weights=True)
 
+    # Tuner.tune(quant_model, torch.optim.SGD, torch.nn.CrossEntropyLoss(), dataset, 5, 10, 10)
 
+    # trainer = Trainer()
+    # trainer.train(quant_model, None, dataset, torch.nn.CrossEntropyLoss(), torch.optim.SGD(quant_model.parameters()), 0.001)
 
-   # quant_model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/quant_config.yaml", preload_weights=True)
-   # model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/config.yaml", preload_weights=True)
-
-    #Tuner.tune(quant_model, torch.optim.SGD, torch.nn.CrossEntropyLoss(), dataset, 5, 10, 10)
-
-    #trainer = Trainer()
-    #trainer.train(quant_model, None, dataset, torch.nn.CrossEntropyLoss(), torch.optim.SGD(quant_model.parameters()), 0.001)
-
-    #validator.validate(quant_model, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
-   # validator.validate(model, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
+    # validator.validate(quant_model, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
+    validator.validate(model, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
