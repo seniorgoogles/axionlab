21c14f9b17db4a8b3c7afcc57e96b1236de81f10
carelledjuffo <djuffocarelle@yahoo.com>
2024-04-01 13:42:36 +0200

    - added mobilenet_v3_large

--- main.py
+++ main.py
@@ -12,12 +12,20 @@ from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    modelbuilder = ModelBuilder()
-    dataset = DatasetBuilder.build(DatasetTypes.IMAGENET, "configs/vgg19/quant_config.yaml")
-    validator = Validator()
+   # modelbuilder = ModelBuilder()
+   # dataset = DatasetBuilder.build(DatasetTypes.IMAGENET, "configs/vgg19/quant_config.yaml")
+   # validator = Validator()
+   mobilenet = models.mobilenet_v3_large(pretrained=True)
+   vgg = models.vgg19(pretrained=True)
+   resnet18 = models.resnet18(pretrained=True)
 
-    quant_model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/quant_config.yaml", preload_weights=True)
-    model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/config.yaml", preload_weights=True)
+   # print(mobilenet)
+   print(resnet18)
+
+
+
+   # quant_model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/quant_config.yaml", preload_weights=True)
+   # model = modelbuilder.build(ModelTypes.VGG, "configs/vgg19/config.yaml", preload_weights=True)
 
     #Tuner.tune(quant_model, torch.optim.SGD, torch.nn.CrossEntropyLoss(), dataset, 5, 10, 10)
 
@@ -25,4 +33,4 @@ if __name__ == "__main__":
     #trainer.train(quant_model, None, dataset, torch.nn.CrossEntropyLoss(), torch.optim.SGD(quant_model.parameters()), 0.001)
 
     #validator.validate(quant_model, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
-    validator.validate(model, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
+   # validator.validate(model, None, dataset.get_test_loader(), torch.nn.CrossEntropyLoss())
