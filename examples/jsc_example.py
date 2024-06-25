ff225fe89ef87bb0706e3f029f52654f5c793fed
mmecik <michael.mecik@informatik.hs-fulda.de>
2024-06-25 10:53:51 +0200

    * JSC models fixed

--- examples/jsc_example.py
+++ examples/jsc_example.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     dataset = DatasetBuilder.build(DatasetTypes.JSC, "../configs/jsc/jsc_xl.yaml")
 
     configs = [
-        "../configs/jsc/jsc-2l.yaml",
-        "../configs/jsc/jsc-5l.yaml",
+        "../configs/jsc/jsc_2l.yaml",
+        "../configs/jsc/jsc_5l.yaml",
         "../configs/jsc/jsc_lite.yaml",
         "../configs/jsc/jsc_m_lite_floating_point.yaml",
         "../configs/jsc/jsc_xl.yaml",
@@ -32,27 +32,9 @@ if __name__ == "__main__":
     ]
 
     for config in configs:
-
-
         try:
-            inp = torch.randn(1, 3, 32, 32)
             model = modelbuilder.build(ModelTypes.JSC, config, preload_weights=True)
-
-
-            def hook_fn(module, grad_input, grad_output):
-                if any(g is None for g in grad_input):
-                    print(f"Gradient input is None for {module}")
-
-
-            model.apply(lambda module: module.register_backward_hook(hook_fn))
-
-            x = model(inp)
-            x.backward(torch.ones_like(x))  # This assumes the model output and labels are the same shape
-
-            for name, param in model.named_parameters():
-                if not param.requires_grad:
-                    print(f"Parameter {name} does not require gradients.")
+            trainer.train(model, None, dataset, torch.nn.CrossEntropyLoss(), torch.optim.Adam(model.parameters(), lr), lr,epochs)
         except Exception as e:
-            print(f"Error in {config}: {e}")
+            print(f"Error: {e}")
             continue
-        #trainer.train(model, None, dataset, torch.nn.CrossEntropyLoss(), torch.optim.Adam(model.parameters(), lr), lr,epochs)
